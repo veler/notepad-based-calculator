436b5fdbdcae9fcc6013209ff25b935157456a81
Etienne Baudoux <etbaudou@microsoft.com>
2022-09-15 17:08:13 -0700

    Updated build

---
+++ src/build/AppVersion/AppxManifestUpdater.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+internal sealed class AppxManifestUpdater
+{
+    private readonly string _versionRule;
+
+    internal AppxManifestUpdater(string versionRule)
+    {
+        _versionRule = versionRule;
+    }
+
+    public void UpdateFile(string fileName)
+    {
+        string text = File.ReadAllText(fileName);
+
+        string ouputText = UpdateTextWithRule(text);
+
+        File.WriteAllText(fileName, ouputText);
+    }
+
+    public string UpdateTextWithRule(string text)
+    {
+        VersionString v = null;
+
+        Tuple<string, int, int> g = GetVersionString(text);
+        if (g != null)
+        {
+            VersionString.TryParse(g.Item1, out v);
+        }
+        if (v != null)
+        {
+            string newVersion = new VersionUpdateRule(_versionRule).Update(v);
+            return text.Substring(0, g.Item2) + newVersion + text.Substring(g.Item2 + g.Item3);
+        }
+        return text;
+    }
+
+    public static Tuple<string, int, int> GetVersionString(string input)
+    {
+        string identityTagStart = "<Identity";
+        string identityTagEnd = ">";
+        string versionAttributeStart = "Version=\"";
+        string versionAttributeEnd = "\"";
+
+        int identityTagStartPosition = input.IndexOf(identityTagStart);
+        if (identityTagStartPosition > -1)
+        {
+            int identityTagEndPosition = input.IndexOf(identityTagEnd, identityTagStartPosition + identityTagStart.Length);
+            if (identityTagEndPosition > -1)
+            {
+                int versionAttributeStartPosition = input.IndexOf(versionAttributeStart, identityTagStartPosition + identityTagStart.Length);
+                if (versionAttributeStartPosition > identityTagStartPosition && versionAttributeStartPosition < identityTagEndPosition)
+                {
+                    int versionAttributeEndPosition = input.IndexOf(versionAttributeEnd, versionAttributeStartPosition + versionAttributeStart.Length);
+
+                    if (versionAttributeEndPosition > versionAttributeStartPosition && versionAttributeEndPosition < identityTagEndPosition)
+                    {
+                        string oldVersion = input.Substring(versionAttributeStartPosition + versionAttributeStart.Length, versionAttributeEndPosition - (versionAttributeStartPosition + versionAttributeStart.Length));
+                        return new Tuple<string, int, int>(
+                            oldVersion,
+                            versionAttributeStartPosition + versionAttributeStart.Length,
+                            oldVersion.Length);
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
--- src/build/Build.cs
+++ src/build/Build.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Linq;
-using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
+﻿using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using static AppVersion;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
@@ -19,26 +17,45 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution]
+    readonly Solution Solution;
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            RootDirectory.GlobDirectories("bin", "obj").ForEach(DeleteDirectory);
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution)
+                .SetVerbosity(DotNetVerbosity.Quiet));
+        });
+
+    Target SetVersion => _ => _
+        .Before(Compile)
+        .Requires(() => Configuration == Configuration.Debug)
+        .Executes(() =>
+        {
+            SetAppVersion(RootDirectory);
         });
 
     Target Compile => _ => _
-        .DependsOn(Restore)
+        .DependsOn(SetVersion)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetVerbosity(DotNetVerbosity.Quiet)
+                .EnableNoRestore());
         });
-
 }
